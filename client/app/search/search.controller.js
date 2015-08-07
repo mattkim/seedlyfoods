31c922b5e19cdc31949956b290decfd111c7fcb7
mattkim <matthkim@gmail.com>
2015-08-07 00:06:00 -0700

    Adding a searchbox

---
+++ client/app/search/search.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('seedlyApp')
+  .controller('SearchCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -14,6 +14,8 @@ angular.module('seedlyApp')
     $scope.isSeller = Auth.isSeller;
     $scope.getCurrentUser = Auth.getCurrentUser;
 
+    $scope.searchbox = {};
+
     $scope.logout = function() {
       Auth.logout();
       $location.path('/login');
@@ -21,5 +23,14 @@ angular.module('seedlyApp')
 
     $scope.isActive = function(route) {
       return route === $location.path();
+    };
+
+    $scope.search = function(form) {
+      // should change the contents of the main page.
+      if(form.$valid){
+        console.log($scope.searchbox);
+        // Needs to redirect to a search view?
+        $location.path('/search');
+      }
     };
   });
