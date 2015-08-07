47c406d206f6c1f6b0283d6b5094d8714eba16d4
mattkim <matthkim@gmail.com>
2015-08-06 21:12:09 -0700

    Adding dev config

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -17,6 +17,12 @@ module.exports = {
     secret: process.env.AWS_SECRET_KEY ||
             'nosecretkey',
     bucket: 'seedlyfoods'
-  }
+  },
 
+  stripe: {
+    sk: process.env.STRIPE_SK ||
+        'nosk',
+    pk: process.env.STRIPE_PK ||
+        'nopk'
+  }
 };
